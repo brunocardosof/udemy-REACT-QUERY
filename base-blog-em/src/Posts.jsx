ee96612e06ae7ba4bdf82c50940ff8aadeded693
brunocardosof <bruno.cardosof@gmail.com>
2023-10-30 18:17:05 -0300

    query keys
    pagination
    
    mutation

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,26 +1,41 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 import { PostDetail } from "./PostDetail";
-const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(currentPage) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${currentPage}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+  const pageLimit = 10;
+
+  const queryClient = useQueryClient();
 
   // replace with useQuery
-  const data = [];
+  const { data, isError, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      keepPreviousData: true,
+    }
+  );
+  useEffect(() => {
+    const nextPage = currentPage + 1;
+    queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+  }, [currentPage]);
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Something went wrong!</p>;
 
   return (
     <>
       <ul>
-        {data.map((post) => (
+        {data?.map((post) => (
           <li
             key={post.id}
             className="post-title"
@@ -31,11 +46,17 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage < 1}
+          onClick={() => setCurrentPage(currentPage - 1)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= pageLimit}
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
           Next page
         </button>
       </div>
